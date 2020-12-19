b03836bc8aadfcbc20dc8914a27bc5cdf8bbe8cd
Ohtani <v24kuong@gmail.com>
2020-12-20 01:17:18 +0900

    Java5

---
+++ src/Chapter05.java
@@ -0,0 +1,19 @@
+
+public class Chapter05 {
+	public static void main(String[] arga) {
+		long l = 123456789L;
+		System.out.println(l);
+		double d = 12345.678d;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+		Float F = 123.456f;
+		System.out.println(F);
+		Integer I = 123;
+		System.out.println(I);
+		Character C = '大';
+		System.out.println(C);
+		int[] array = { 1, 2, 3, 4, 5 };
+		System.out.println(array[3]);
+	}
+}
